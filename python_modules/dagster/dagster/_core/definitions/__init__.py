98b8c415ff11a40454bb018ad2baf0441ad780a8
Claire Lin <claire@elementl.com>
2023-03-13 17:51:35 -0700

    updates

--- python_modules/dagster/dagster/_core/definitions/__init__.py
+++ python_modules/dagster/dagster/_core/definitions/__init__.py
@@ -169,6 +169,7 @@ from .partition_mapping import (
     AllPartitionMapping as AllPartitionMapping,
     IdentityPartitionMapping as IdentityPartitionMapping,
     LastPartitionMapping as LastPartitionMapping,
+    MultiPartitionsMapping as MultiPartitionsMapping,
     MultiToSingleDimensionPartitionMapping as MultiToSingleDimensionPartitionMapping,
     PartitionMapping as PartitionMapping,
 )
